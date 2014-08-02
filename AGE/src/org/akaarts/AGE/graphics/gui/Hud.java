51199b2db535c1a8755d8f51c7b83aa622491fac
Aka-Arts <alphaegli@gmail.com>
2014-08-02 18:00:52 +0200

    Improve Hud classes

--- AGE/src/org/akaarts/AGE/graphics/gui/Hud.java
+++ AGE/src/org/akaarts/AGE/graphics/gui/Hud.java
@@ -1,5 +1,35 @@
 package org.akaarts.AGE.graphics.gui;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Hud {
+	
+	private static ArrayList<HudElement> hudElements = new ArrayList<HudElement>(30);
+	private static Iterator<HudElement> iterator = hudElements.iterator();
+	
+	private Hud(){}
+	
+	public static void draw(){
+		while(iterator.hasNext()){
+			HudElement elem = iterator.next();
+			if(elem!=null){
+				elem.draw();
+			}
+		}
+	}
+	
+	public static int addElement(HudElement elem){
+		if(hudElements.contains(elem)){
+			return hudElements.indexOf(elem);
+		}
+		hudElements.add(elem);
+		iterator = hudElements.iterator();
+		return hudElements.indexOf(elem);
+	}
+	
+	public static void deleteElement(int id){
+		hudElements.remove(id);
+	}
 	
 }
---
+++ AGE/src/org/akaarts/AGE/graphics/gui/HudElement.java
@@ -0,0 +1,15 @@
+package org.akaarts.AGE.graphics.gui;
+
+import org.akaarts.AGE.Console;
+
+public class HudElement {
+	
+	public HudElement(){
+		
+	}
+	
+	public void draw(){
+		Console.info("Draw element!");
+	}
+	
+}
