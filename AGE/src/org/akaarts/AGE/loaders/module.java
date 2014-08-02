722a97a3c7fc01599d12e88cb08f7edbdf3fc64e
Lucas Egli <Aka-Arts@users.noreply.github.com>
2014-08-02 11:00:09 +0200

    Update and rename modulLoader.java to module.java
    Will be the baseclass for AGE-modules

---
+++ AGE/src/org/akaarts/AGE/loaders/module.java
@@ -0,0 +1,21 @@
+package org.akaarts.AGE.loaders;
+
+public abstract class module{
+	
+	public module(){
+		
+	}
+	
+	public start(){
+		
+	}
+	
+	private loop(){
+		
+	}
+	
+	private stop{
+		
+	}
+	
+}
