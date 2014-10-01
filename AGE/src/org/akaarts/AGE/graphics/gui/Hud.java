a58cbeaecfe38adfb0c56463d56116ec4c43c8fc
Aka-Arts <alphaegli@gmail.com>
2014-10-01 14:18:22 +0200

    add root element

--- AGE/src/org/akaarts/AGE/graphics/gui/Hud.java
+++ AGE/src/org/akaarts/AGE/graphics/gui/Hud.java
@@ -2,5 +2,6 @@ package org.akaarts.AGE.graphics.gui;
 
 public class Hud {
 	
+	 private static final HudElement ROOT = HudElement.getRoot();
 	
 }
--- AGE/src/org/akaarts/AGE/graphics/gui/HudElement.java
+++ AGE/src/org/akaarts/AGE/graphics/gui/HudElement.java
@@ -1,5 +1,7 @@
 package org.akaarts.AGE.graphics.gui;
 
+import java.util.ArrayList;
+
 public class HudElement{
 
 	private static HudElement root = new HudElement();
@@ -7,24 +9,77 @@ public class HudElement{
 	private String widthExpression, heightExpression;
 	private int width,height;
 	
-	private String position;
+	private String positionXExpression, positionYExpression;	
+	private String originX,	originY;
+	private int positionX,positionY;
 	
-	private String topExpression, leftExpression, bottomExpression, rightExpression;
-	private int top, left, bottom, right;
+	private String backgroundColorExpression, backgroundImageExpression, backgroundFilterExpression, backgroundUVExpression, backgroundRepeatExpression;
+	private float backgroundColorR, backgroundColorG, backgroundColorB, backgroundColorA;
+	
+	public final boolean ISROOT;
+	
+	private ArrayList<HudElement> children = new ArrayList<HudElement>();
+	
+	public static final String ORIGIN_CENTER = "center",
+							ORIGIN_TOP = "top", 
+							ORIGIN_BOTTOM = "bottom", 
+							ORIGIN_LEFT = "left", 
+							ORIGIN_RIGHT = "right";
 	
 	/**
 	 * Constructor only for the root element
 	 * Setting all default styles
 	 */
 	private HudElement() {
+		
+		this.ISROOT = true;
+		
+		this.applyDefaults();
+		
+		
+	}
+	
+	public void draw() {
+		
+	}
+	
+	/**
+	 * Recalculates the element and it's children
+	 */
+	public void update() {
+
+		//TODO update here...
+		
+		//update all children
+		
+		for(HudElement child:this.children){
+			child.update();
+		}
+		
+	}
+	
+	public static HudElement getRoot() {
+		return root;
+	}
+	
+	/**
+	 * applies all the default expressions
+	 */
+	private void applyDefaults(){
 		this.widthExpression = "100%";
 		this.heightExpression = "100%";
 		
-		this.position = "static";
-	}
-	
-	public HudElement getRoot() {
-		return root;
+		this.originX = ORIGIN_CENTER;
+		this.originY = ORIGIN_CENTER;
+		
+		this.positionXExpression = "0px";
+		this.positionYExpression = "0px";
+		
+		this.backgroundColorExpression = "#00000000";
+		this.backgroundImageExpression = "none";
+		this.backgroundFilterExpression = "nearest";
+		this.backgroundRepeatExpression = "none";
+		this.backgroundUVExpression = "0 0,0 1,1 1,1 0";
 	}
 
 	
